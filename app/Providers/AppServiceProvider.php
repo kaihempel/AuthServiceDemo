c8c39d001cf8eb0a898287b21c8e3b79847b726f
Kai Hempel <kai.hempel@kuweh.de>
2017-03-22 22:04:29 +0100

    Facebook provider added to dependency injection container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Model\Facebook\FacebookFactory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->instance('App\Model\Facebook\OAuth\FacebookProvider', $this->initializeFacebookProvider());
+    }
+
+    /**
+     * Initialize the facebook oauth provider
+     *
+     * @return \App\Model\Facebook\OAuth\FacebookProvider
+     */
+    private function initializeFacebookProvider()
+    {
+        $clientId       = config('services.facebook.client_id');
+        $clientSecret   = config('services.facebook.client_secret');
+        $callbackUrl    = config('services.facebook.callback_url');
+        $session        = $this->app->session->driver();
+
+        $factory = new FacebookFactory($clientId, $clientSecret, $callbackUrl, $session);
+        return $factory->getProvider();
     }
 }
